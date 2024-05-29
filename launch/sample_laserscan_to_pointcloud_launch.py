2cf66fd78435cab488fd776cb18aa5c208465637
MasakiFujiwara <fmasaki2020@gmail.com>
2024-05-29 17:20:09 +0900

    update for test

--- launch/sample_laserscan_to_pointcloud_launch.py
+++ launch/sample_laserscan_to_pointcloud_launch.py
@@ -9,35 +9,43 @@ import yaml
 
 def generate_launch_description():
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='scanner', default_value='scanner',
-            description='Namespace for sample topics'
-        ),
-        ExecuteProcess(
-            cmd=[
-                'ros2', 'topic', 'pub', '-r', '10',
-                '--qos-profile', 'sensor_data',
-                [LaunchConfiguration(variable_name='scanner'), '/scan'],
-                'sensor_msgs/msg/LaserScan', yaml.dump({
-                    'header': {'frame_id': 'scan'}, 'angle_min': -1.0,
-                    'angle_max': 1.0, 'angle_increment': 0.1, 'range_max': 10.0,
-                    'ranges': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-                })
-            ],
-            name='scan_publisher'
-        ),
-        Node(
-            package='tf2_ros',
-            executable='static_transform_publisher',
-            name='static_transform_publisher',
-            arguments='0 0 0 0 0 0 1 map scan'
-        ),
+        # DeclareLaunchArgument(
+        #     name='scanner', default_value='scanner',
+        #     description='Namespace for sample topics'
+        # ),
+        # ExecuteProcess(
+        #     cmd=[
+        #         'ros2', 'topic', 'pub', '-r', '10',
+        #         '--qos-profile', 'sensor_data',
+        #         [LaunchConfiguration(variable_name='scanner'), '/scan'],
+        #         'sensor_msgs/msg/LaserScan', yaml.dump({
+        #             'header': {'frame_id': 'scan'}, 'angle_min': -1.0,
+        #             'angle_max': 1.0, 'angle_increment': 0.1, 'range_max': 10.0,
+        #             'ranges': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        #         })
+        #     ],
+        #     name='scan_publisher'
+        # ),
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='static_transform_publisher',
+        #     arguments='0 0 0 0 0 0 1 map scan'
+        # ),
+        # Node(
+        #     package='pointcloud_to_laserscan',
+        #     executable='laserscan_to_pointcloud_node',
+        #     name='laserscan_to_pointcloud',
+        #     remappings=[('scan_in', [LaunchConfiguration(variable_name='scanner'), '/velodyne_2dscan_high_beams']),
+        #                 ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
+        #     parameters=[{'target_frame': 'velodyne', 'transform_tolerance': 0.01}]
+        # ),
         Node(
             package='pointcloud_to_laserscan',
             executable='laserscan_to_pointcloud_node',
             name='laserscan_to_pointcloud',
-            remappings=[('scan_in', [LaunchConfiguration(variable_name='scanner'), '/scan']),
-                        ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'target_frame': 'scan', 'transform_tolerance': 0.01}]
+            remappings=[('scan_in', '/velodyne_2dscan_high_beams'),
+                        ('cloud', '/cloud')],
+            parameters=[{'target_frame': 'velodyne', 'transform_tolerance': 0.01}]
         ),
     ])
